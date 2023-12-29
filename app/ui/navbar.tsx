0fb2a7ad788780ff66d71c2b8172226918a5921d
PakornBank <pakorn22120@gmail.com>
2023-12-30 04:05:18 +0700

    Add user avatar and name to navbar

--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -1,18 +1,30 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { useSession, signIn, signOut } from "next-auth/react";
 import {
   IconGauge,
   IconFingerprint,
   IconActivity,
   IconChevronRight,
 } from "@tabler/icons-react";
-import { Button, Flex, NavLink, Stack, Autocomplete } from "@mantine/core";
+import {
+  Button,
+  Flex,
+  NavLink,
+  Stack,
+  Autocomplete,
+  Group,
+  Avatar,
+} from "@mantine/core";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useForm } from "@mantine/form";
 
 export default function NavbarSimple() {
+  const { data: session, status } = useSession();
+  const userName = session?.user?.name;
+  const userIcon = session?.user?.image;
   const form = useForm({
     initialValues: { category: "" },
   });
@@ -69,13 +81,29 @@ export default function NavbarSimple() {
         </form>
 
         <Stack gap={0}>
-          <NavLink
-            component={Link}
-            href="#required-for-focus"
-            label="Logout"
-            rightSection={<IconChevronRight size="1rem" stroke={1.5} />}
-            color="gray"
-          />
+          {status === "authenticated" && (
+            <Group wrap="nowrap">
+              <Avatar variant="filled" radius={"xl"} src={userIcon}></Avatar>
+              <NavLink
+                component={Link}
+                href="/login"
+                label={userName}
+                rightSection={<IconChevronRight size="1rem" stroke={1.5} />}
+                color="gray"
+              />
+            </Group>
+          )}
+          {status === "authenticated" ? (
+            <Button onClick={() => signOut()}>Sign out</Button>
+          ) : (
+            <NavLink
+              component={Link}
+              href="/login"
+              label="Login"
+              rightSection={<IconChevronRight size="1rem" stroke={1.5} />}
+              color="gray"
+            />
+          )}
         </Stack>
       </Flex>
     </>
