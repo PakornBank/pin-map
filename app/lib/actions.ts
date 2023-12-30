de57afb4ae486fb9f41f3e94abac9866ba1bb909
PakornBank <pakorn22120@gmail.com>
2023-12-30 17:28:44 +0700

    Add createUser function to insert user data into the database

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,1 +1,14 @@
 "use server";
+
+import { sql } from "@vercel/postgres";
+
+export async function createUser(email: string, name: string, image: string) {
+  try {
+    const data =
+      await sql`INSERT INTO users (email, name, image) VALUES (${email}, ${name}, ${image}) RETURNING *`;
+    return data.rows[0];
+  } catch (error) {
+    console.error("Failed to create user:", error);
+    return null;
+  }
+}
