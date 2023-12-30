d7e9bb495572c985c0e3887a4d5bbee1430fb05b
PakornBank <pakorn22120@gmail.com>
2023-12-30 17:28:36 +0700

    Add GitHub sign-in callback to create user if not found

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,38 @@
-import NextAuth from "next-auth";
+import NextAuth, { AuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { findUserByEmail } from "@/app/lib/data";
+import { createUser } from "@/app/lib/actions";
 
-export const authOptions = {
+export const authOptions: AuthOptions = {
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID ?? "",
       clientSecret: process.env.GITHUB_SECRET ?? "",
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile }): Promise<string | boolean> {
+      if (account?.provider === "github") {
+        const email = user.email;
+        let existingUser = await findUserByEmail(email ?? "");
+        if (!existingUser) {
+          console.log("Creating user:", email);
+          existingUser = await createUser(
+            email ?? "",
+            user.name ?? "",
+            user.image ?? ""
+          );
+        }
+        if (existingUser) {
+          console.log("User found:", existingUser);
+          user.id = existingUser.id;
+          return true;
+        }
+      }
+      console.log("Failed to sign in:", user);
+      return false;
+    },
+  },
 };
 
 export const handler = NextAuth(authOptions);
