bcfd1a976ceb549cb0867752e4b489f23372f025
PakornBank <pakorn22120@gmail.com>
2024-01-02 02:47:58 +0700

    Add PopupCardEdit component and implement editing functionality

--- app/ui/popup-card.tsx
+++ app/ui/popup-card.tsx
@@ -1,27 +1,53 @@
 import PopupCardDisplay from "./popup-card-display";
 import PopupCardDelete from "./popup-card-delete";
 import { useSession } from "next-auth/react";
+import PopupCardEdit from "./popup-card-edit";
+import { Group, Button } from "@mantine/core";
+import { useState } from "react";
 
 export default function PopupCard({
   popupInfo,
   fetchPins,
+  setPopupInfo,
 }: {
   popupInfo: any;
   fetchPins: () => void;
+  setPopupInfo: (info: any) => void;
 }) {
+  const [isEditing, setIsEditing] = useState(false);
   console.log(popupInfo);
   const { data: session, status } = useSession();
   return (
     <>
-      <PopupCardDisplay popupInfo={popupInfo} />
-      {status === "authenticated" &&
-        session?.user?.id === popupInfo.user_id && (
-          <PopupCardDelete
-            popupId={popupInfo.id}
-            userId={session.user.id}
+      {isEditing &&
+      status === "authenticated" &&
+      session?.user?.id === popupInfo.user_id ? (
+        <>
+          <PopupCardEdit
+            pinData={popupInfo}
             fetchPins={fetchPins}
-          />
-        )}
+            setIsEditing={setIsEditing}
+            setPopupInfo={setPopupInfo}
+          ></PopupCardEdit>
+          {/* <Button onClick={() => setIsEditing(false)}>Cancel</Button> */}
+        </>
+      ) : (
+        <>
+          <PopupCardDisplay popupInfo={popupInfo} />
+          {status === "authenticated" &&
+            session?.user?.id === popupInfo.user_id && (
+              <Group>
+                <PopupCardDelete
+                  popupId={popupInfo.id}
+                  userId={session.user.id}
+                  fetchPins={fetchPins}
+                  setPopupInfo={setPopupInfo}
+                />
+                <Button onClick={() => setIsEditing(true)}>Edit</Button>
+              </Group>
+            )}
+        </>
+      )}
     </>
   );
 }
