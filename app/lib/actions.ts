f62aab83fed863cdbb424f11b0aecaea6751080a
PakornBank <pakorn22120@gmail.com>
2024-01-01 21:32:51 +0700

    Add createPin function to actions.ts

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -12,3 +12,33 @@ export async function createUser(email: string, name: string, image: string) {
     return null;
   }
 }
+
+export async function createPin({
+  image_url,
+  pin_name,
+  category,
+  is_active,
+  description,
+  user_id,
+  latitude,
+  longitude,
+}: {
+  image_url: string;
+  pin_name: string;
+  category: string;
+  is_active: boolean;
+  description: string;
+  latitude: number;
+  longitude: number;
+  user_id: number;
+}) {
+  try {
+    const data =
+      await sql`INSERT INTO pins (image_url, pin_name, category, is_active, description, user_id, latitude, longitude) VALUES (${image_url}, ${pin_name}, ${category}, ${is_active}, ${description}, ${user_id}, ${latitude}, ${longitude}) RETURNING *`;
+    console.log(data);
+    return data.rows[0];
+  } catch (error) {
+    console.error("Failed to create pin:", error);
+    return null;
+  }
+}
