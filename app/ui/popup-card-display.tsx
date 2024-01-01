0d8e816d2e6c2fb55218bcf40969cbb16e332a13
PakornBank <pakorn22120@gmail.com>
2024-01-01 23:49:18 +0700

    Add PopupCardDisplay component

---
+++ app/ui/popup-card-display.tsx
@@ -0,0 +1,38 @@
+import { Card, Image, Text, Badge, Group } from "@mantine/core";
+import { formatDateTime } from "../lib/utils";
+
+export default function PopupCardDisplay({ popupInfo }: { popupInfo: any }) {
+  console.log(popupInfo);
+  return (
+    <Card shadow="sm" padding="md" radius="md" withBorder>
+      <Card.Section>
+        <Image
+          src={popupInfo.image_url}
+          alt={popupInfo.pin_name}
+          height={150}
+          width={300}
+          fit="cover"
+        />
+      </Card.Section>
+      <Text fw={500}>{popupInfo.pin_name}</Text>
+      <Group gap={5}>
+        <Badge color="pink">{popupInfo.category}</Badge>
+        {popupInfo.is_active && <Badge color="green">Active</Badge>}
+        {popupInfo.tag &&
+          popupInfo.tag.map((tag: string, index: number) => (
+            <Badge key={`tag${index}of${popupInfo.id}`}>{tag}</Badge>
+          ))}
+      </Group>
+
+      <Text size="sm" c="dimmed">
+        {popupInfo.description}
+      </Text>
+      <Text size="sm" c="dimmed">
+        updated {formatDateTime(popupInfo.updated_at)}
+      </Text>
+      <Text size="sm" c="dimmed">
+        by {popupInfo.name}
+      </Text>
+    </Card>
+  );
+}
