b0b02a2f12be4f2d0e275bca309908c614bdfdb6
PakornBank <pakorn22120@gmail.com>
2024-01-01 21:32:35 +0700

    Add noStore() function to GET request

--- app/api/category/route.ts
+++ app/api/category/route.ts
@@ -1,6 +1,8 @@
 import { sql } from "@vercel/postgres";
+import { unstable_noStore as noStore } from "next/cache";
 
 export async function GET(request: Request) {
+  noStore();
   try {
     const data = await sql`SELECT DISTINCT category FROM pins`;
     return Response.json(data.rows);
