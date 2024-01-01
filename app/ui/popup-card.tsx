b07fdd965cfeac898648c8a3e638a903e2e5b2da
PakornBank <pakorn22120@gmail.com>
2024-01-01 23:49:11 +0700

    Refactor PopupCard component

--- app/ui/popup-card.tsx
+++ app/ui/popup-card.tsx
@@ -1,37 +1,27 @@
-import { Card, Image, Text, Badge, Group } from "@mantine/core";
-import { formatDateTime } from "../lib/utils";
+import PopupCardDisplay from "./popup-card-display";
+import PopupCardDelete from "./popup-card-delete";
+import { useSession } from "next-auth/react";
 
-export default function PopupCard({ popupInfo }: { popupInfo: any }) {
+export default function PopupCard({
+  popupInfo,
+  fetchPins,
+}: {
+  popupInfo: any;
+  fetchPins: () => void;
+}) {
+  console.log(popupInfo);
+  const { data: session, status } = useSession();
   return (
-    <Card shadow="sm" padding="md" radius="md" withBorder>
-      <Card.Section>
-        <Image
-          src={popupInfo.image_url}
-          alt={popupInfo.pin_name}
-          height={150}
-          width={300}
-          fit="cover"
-        />
-      </Card.Section>
-      <Text fw={500}>{popupInfo.pin_name}</Text>
-      <Group gap={5}>
-        <Badge color="pink">{popupInfo.category}</Badge>
-        {popupInfo.is_active && <Badge color="green">Active</Badge>}
-        {popupInfo.tag &&
-          popupInfo.tag.map((tag: string, index: number) => (
-            <Badge key={`tag${index}of${popupInfo.id}`}>{tag}</Badge>
-          ))}
-      </Group>
-
-      <Text size="sm" c="dimmed">
-        {popupInfo.description}
-      </Text>
-      <Text size="sm" c="dimmed">
-        updated {formatDateTime(popupInfo.updated_at)}
-      </Text>
-      <Text size="sm" c="dimmed">
-        by {popupInfo.name}
-      </Text>
-    </Card>
+    <>
+      <PopupCardDisplay popupInfo={popupInfo} />
+      {status === "authenticated" &&
+        session?.user?.id === popupInfo.user_id && (
+          <PopupCardDelete
+            popupId={popupInfo.id}
+            userId={session.user.id}
+            fetchPins={fetchPins}
+          />
+        )}
+    </>
   );
 }
