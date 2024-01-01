5faa838777810d71db0509e6a83fef57df87ee97
PakornBank <pakorn22120@gmail.com>
2024-01-02 02:57:50 +0700

    Refactor control panel component

--- app/ui/control-panel.tsx
+++ app/ui/control-panel.tsx
@@ -10,36 +10,10 @@ import {
   NavLink,
 } from "@mantine/core";
 import Link from "next/link";
-import { useForm } from "@mantine/form";
 import { useSession } from "next-auth/react";
-import redirect from "next/navigation";
 
 function round5(value: number) {
   return (Math.round(value * 1e5) / 1e5).toFixed(5);
-}
-
-function isValidLat(value: number) {
-  if (value < -90 || value > 90) {
-    // return "Latitude must be between -90 and 90";
-    return false;
-  }
-  if (isNaN(value)) {
-    //   return "Latitude must be a number";
-    return false;
-  }
-  return true;
-}
-
-function isValidLong(value: number) {
-  if (value < -180 || value > 180) {
-    // return "Longitude must be between -180 and 180";
-    return false;
-  }
-  if (isNaN(value)) {
-    //
-    return false;
-  }
-  return true;
 }
 
 interface ControlPanelProps {
@@ -60,16 +34,6 @@ function ControlPanel(props: ControlPanelProps) {
     handleChangeLong,
   } = props;
 
-  // const form = useForm({
-  //   validateInputOnChange: true,
-  //   initialValues: { latitude: latitude, longitude: longitude },
-  //   // functions will be used to validate values at corresponding key
-  //   validate: {
-  //     latitude: validateLatitude,
-  //     longitude: validateLongitude,
-  //   },
-  // });
-
   return status === "authenticated" ? (
     <Stack
       className="control-panel"
@@ -82,7 +46,7 @@ function ControlPanel(props: ControlPanelProps) {
         Pin location
       </Text>
       <Text size="md">Drag the screen around</Text>
-      <NumberInput
+      {/* <NumberInput
         value={round5(latitude)}
         onChange={(newValue) => {
           if (isValidLat(+newValue)) {
@@ -104,9 +68,13 @@ function ControlPanel(props: ControlPanelProps) {
         label="Longitude"
         hideControls
         // {...form.getInputProps("longitude")}
-      />
+      /> */}
+      <Text size="sm">Latitude</Text>
+      <Text size="md">{round5(latitude)}</Text>
+      <Text size="sm">Longitude</Text>
+      <Text size="md">{round5(longitude)}</Text>
 
-      <Button onClick={handleShowMarker}>toggle marker</Button>
+      <Button onClick={handleShowMarker}>Add a pin</Button>
     </Stack>
   ) : (
     <Button
