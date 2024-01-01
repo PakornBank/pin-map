484ded27e6427fe3090f99b75e2a289a1adb6ed7
PakornBank <pakorn22120@gmail.com>
2024-01-01 21:32:44 +0700

    Add user name to pin data retrieval

--- app/api/pins/route.ts
+++ app/api/pins/route.ts
@@ -1,14 +1,18 @@
 import { sql } from "@vercel/postgres";
+import { unstable_noStore as noStore } from "next/cache";
 
 export async function GET(request: Request) {
+  noStore();
   try {
     const query = new URL(request.url).searchParams;
     const category = query.get("category");
     if (!category) {
-      const data = await sql`SELECT * FROM pins`;
+      const data =
+        await sql`SELECT pins.*, users.name FROM pins INNER JOIN users ON pins.user_id = users.id;`;
       return Response.json(data.rows);
     }
-    const data = await sql`SELECT * FROM pins WHERE category = ${category}`;
+    const data = await sql`SELECT pins.*, users.name FROM pins INNER JOIN 
+    users ON pins.user_id = users.id WHERE category = ${category}`;
     return Response.json(data.rows);
   } catch (error) {
     return Response.json([]);
