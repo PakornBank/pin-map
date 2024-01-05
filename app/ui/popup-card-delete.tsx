204dcce7033eb46c48a6f7ee33af120dc1815cfd
PakornBank <pakorn22120@gmail.com>
2024-01-05 15:14:20 +0700

    Add LoadingOverlay and useDisclosure hook to PopupCardDelete

--- app/ui/popup-card-delete.tsx
+++ app/ui/popup-card-delete.tsx
@@ -1,7 +1,8 @@
 import { IconTrash } from "@tabler/icons-react";
-import { Button } from "@mantine/core";
+import { Button, LoadingOverlay } from "@mantine/core";
 import { deletePin } from "../lib/actions";
 import { useState } from "react";
+import { useDisclosure } from "@mantine/hooks";
 
 export default function PopupCardDelete({
   popupId,
@@ -14,20 +15,24 @@ export default function PopupCardDelete({
   fetchPins: () => void;
   setPopupInfo: (info: any) => void;
 }) {
+  const [visible, { toggle }] = useDisclosure(false);
   const [isDeleting, setIsDeleting] = useState(false);
   const handleDelete = async () => {
+    toggle();
     try {
       const res = await deletePin(popupId, userId);
       // console.log("done!", res);
       setIsDeleting(false);
       setPopupInfo(null);
       fetchPins();
+      toggle();
     } catch (error) {
       console.log(error);
     }
   };
   return (
     <>
+      <LoadingOverlay visible={visible} />
       {isDeleting ? (
         <Button onClick={handleDelete}>Confirm?</Button>
       ) : (
