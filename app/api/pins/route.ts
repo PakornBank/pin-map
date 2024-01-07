4bc124bf351bd58b718329d65603e2686c7c5316
PakornBank <pakorn22120@gmail.com>
2024-01-07 16:43:51 +0700

    Refactor pin retrieval logic to support case-insensitive category search

--- app/api/pins/route.ts
+++ app/api/pins/route.ts
@@ -5,16 +5,16 @@ export async function GET(request: Request) {
   noStore();
   try {
     const query = new URL(request.url).searchParams;
-    const category = query.get("category");
-    if (!category) {
-      const data =
-        await sql`SELECT pins.*, users.name FROM pins INNER JOIN users ON pins.user_id = users.id;`;
-      return Response.json(data.rows);
-    }
-    const data = await sql`SELECT pins.*, users.name FROM pins INNER JOIN 
-    users ON pins.user_id = users.id WHERE category = ${category}`;
+    const category: string = query.get("category") || "";
+    const data = await sql`
+    SELECT pins.*, users.name 
+    FROM pins 
+    INNER JOIN users ON pins.user_id = users.id 
+    WHERE LOWER(category) LIKE LOWER(${"%" + category + "%"})
+    `;
     return Response.json(data.rows);
   } catch (error) {
+    console.error("Failed to fetch pins:", error);
     return Response.json([]);
   }
 }
