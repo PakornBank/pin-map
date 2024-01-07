3e3aa8d682e9e4051895b6b91b42e11db6410be9
PakornBank <pakorn22120@gmail.com>
2024-01-07 16:58:11 +0700

    Add filtering by user and pin name in GET request

--- app/api/pins/route.ts
+++ app/api/pins/route.ts
@@ -6,11 +6,15 @@ export async function GET(request: Request) {
   try {
     const query = new URL(request.url).searchParams;
     const category: string = query.get("category") || "";
+    const user: string = query.get("user") || "";
+    const pin_name: string = query.get("pin_name") || "";
     const data = await sql`
     SELECT pins.*, users.name 
     FROM pins 
     INNER JOIN users ON pins.user_id = users.id 
     WHERE LOWER(category) LIKE LOWER(${"%" + category + "%"})
+    AND LOWER(pin_name) LIKE LOWER(${"%" + pin_name + "%"})
+    AND LOWER(users.name) LIKE LOWER(${"%" + user + "%"})
     `;
     return Response.json(data.rows);
   } catch (error) {
